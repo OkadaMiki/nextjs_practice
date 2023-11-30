9466cfc261c8a795a18143a0e38471a54c8269df
Rikuto <orikutobasuke@gmail.com>
2023-11-30 19:06:57 +0900

    sample03を作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link'
 
-export default function Home(){
-  return(
-      <>
-        <div>
-          <h1>next.js</h1>
-          <Link href='./samples/01'>sample01</Link>
-          <Link href='./samples/02'>sample02</Link>
-        </div>
-      </>
-    )
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h1>next.js</h1>
+        <Link href='./samples/01'>sample01</Link>
+        <Link href='./samples/02'>sample02</Link>
+        <Link href='./samples/03'>sample03</Link>
+      </div>
+    </>
+  )
 }
